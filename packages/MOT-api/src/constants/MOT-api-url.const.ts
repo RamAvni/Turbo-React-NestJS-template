4722ae4d2311c90a2c1f6ac88dcc3fe600742ba9
RamAvni <ramavni@hilma.tech>
2025-10-17 00:33:13 +0300

    feat(MOT-api): some basic routes to check everything can work together, with testing and react-query

---
+++ packages/MOT-api/src/constants/MOT-api-url.const.ts
@@ -0,0 +1,33 @@
+/**
+ * Ministry of Transportation's public api URL
+ * {API_BASE_URL}/{CONTROLLER}/{ENDPOINT}
+ * */
+export const MOT_API_URL = "https://bus.gov.il/WebApi/api";
+
+/**
+ * A list of sections("controllers") in the api
+ * {API_BASE_URL}/{CONTROLLER}/{ENDPOINT}
+ * */
+const MOT_API_CONTROLLERS = {
+  passengerInfo: "passengerInfo",
+} as const;
+
+/**
+ * A list of helper functions that build URL paths based on endpoints in MOT's api
+ * Given PATH is composed of CONTROLLER and ENDPOINT
+ * {API_BASE_URL}/{CONTROLLER}/{ENDPOINT}
+ * */
+export const MOT_API_SERVICES = {
+  /**
+   * get a list of all cities/settelments
+   * NOTE: I did not find any other languages other than Hebrew and English, but there must be more!
+   * */
+  getCityList: (language: "he" | "en") =>
+    `${MOT_API_CONTROLLERS.passengerInfo}/getCityList/${language}/false`,
+
+  getBusStopListByRadius: (latitude: string, longitude: string) =>
+    `${MOT_API_CONTROLLERS.passengerInfo}/GetBusstopListByRadius/1/${latitude}/${longitude}/300/he/false`,
+
+  getRealTimeBusLineListByBusStop: (busStop: string) =>
+    `${MOT_API_CONTROLLERS.passengerInfo}/GetRealtimeBusLineListByBustop/${busStop}/he/false`,
+} as const;
